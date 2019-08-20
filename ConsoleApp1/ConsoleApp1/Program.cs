07cc33649a4b702ae6581203c932d9a2d7d090a9
DORIAN RAYGOZA <doriansamuelraygozaalvarado@gmail.com>
2019-08-20 09:41:06 -0500

    cambie mi pene

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y,z;
+            Console.WriteLine("Impprime los numeros del X hasta Y de -1");
+            Console.WriteLine("Donde arrancamos el ciclo (mayor):");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De donde termiamos el ciclo (menor):");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De cuanto en cuanto:");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=x; i > y; i-=z)
+            {
+                Console.WriteLine("i =" + i);
+            }
+        }
+    }
+}
